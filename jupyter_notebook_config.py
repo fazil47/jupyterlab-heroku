5b6b97f58e5c8ed58c9c3e4076f5fdb8cd7f155e
Fazil Babu <fazil47babu@gmail.com>
2020-10-11 22:34:39 +0530

    p

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,5 +4,5 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
-c.NotebookApp.password="sha1:bca9422a4bbf:6b2c319684b3b95d30245b6b3b9ee307245032bb"
+c.NotebookApp.password="sha1:6301485123be:287f2b43a566a8e00c2cd71abc2d76a6eb207028"
 c.NotebookApp.token=""
